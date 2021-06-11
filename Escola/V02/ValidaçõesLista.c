9b192b73144c28b28c257cd53cd8c77a8b3b43de
Ruan Tarcisio <57304276+RuanTarcisio@users.noreply.github.com>
2021-06-11 10:16:56 -0300

    Update ValidaçõesLista.c
    Correções em remoção, criação da função IsEmpty e pequenas alterações em AtPos.

--- Escola/V02/ValidaçõesLista.c
+++ Escola/V02/ValidaçõesLista.c
@@ -35,23 +35,28 @@ int Push(Node** Geral, cadastro *Pessoas, int select)
             novo->dados.matricula = MATRICULA_PROF;
         *Geral = novo;
         (*Geral)->tamanho = 1;
-        (*Geral)->posicao = 1;
+        (*Geral)->posicao =1;
     }
     else
     {
         novo->prox = *Geral;
         if(select == 1)
         {
-            novo->dados.matricula = MATRICULA_ALUN + (*Geral)->posicao;
+            novo->dados.matricula = (*Geral)->dados.matricula +1;
+            *Geral = novo;
+            (*Geral)->tamanho ++;
+            (*Geral)->posicao ++;
         }
         else
-            novo->dados.matricula = MATRICULA_PROF + (*Geral)->posicao;
+            novo->dados.matricula = (*Geral)->dados.matricula +1;
         *Geral = novo;
         (*Geral)->tamanho ++;
         (*Geral)->posicao ++;
     }
+
     return 1; /* FUTURAS IMPLEMENTAÇÕES CONFIRMAR SE HA ESPAÇO*/
 }
+
 
 void Display(Node *Geral, int select)
 {
@@ -81,68 +86,83 @@ void Display(Node *Geral, int select)
             printf("\n\n%d ", ref->dados.matricula);
             printf("\n\n%s ", ref->dados.nascimento);
             printf("\n\n%s ", ref->dados.cpf);
-            printf("\n\n%s ", ref->dados.sexo);
+            printf("\n \n%c", ref->dados.sexo);
             printf("\n\n ---------------------------------------------------");
             ref = ref->prox;
         }
         while(ref != NULL);
     }
     printf("\n\n");
-    free(ref);
+
 }
+
 
 int Remove(Node **Geral, int select)
 {
-    Node *cache = malloc(sizeof(Node));
-    Node *next = malloc(sizeof(Node));
+    Node *cache = NULL, *next = NULL;
+    int validador;
 
-    cache = AtPos(*Geral, select);
+    setbuf(stdin,NULL);
 
+        if(*Geral == NULL)
+        {
+            validador = 0;
+        }
 
-    if(*Geral == NULL)
-    {
-        return -1;
-    }
-    else
-    {
-        if(cache == *Geral)
+        else if(*Geral != NULL)
         {
-            *Geral = cache->prox;
-            (*Geral)->tamanho--;
-            free(cache);
-            return 1;
+            cache = AtPos(*Geral, select);
+
+            if(cache == NULL)
+            {
+                validador = -1;
+            }
+
+            else if(cache->prox == NULL)
+            {
+                if(isEmpty(cache)== 1)
+                {
+                    *Geral = criar_Lista();
+                }
+                validador = 1;
+            }
+            else if(cache->prox != NULL)
+            {
+                next = cache->prox;
+                cache->prox = next ->prox;
+                (*Geral)->tamanho--;
+                validador = 1;
+            }
         }
-        else if(cache == NULL)
-        {
-            return -2;
-        }
-        else
-        {
-            next = cache->prox;
-            cache->prox = next->prox;
-            free(next);
-            (*Geral)->tamanho--;
-        }
-    }
-    return 1;
+
+    free(next);
+    return validador;
 }
 
-void pop(Node** Geral)
+
+void Pop(Node** Geral)
 {
 
     Node* cache = (Node*) malloc(sizeof(Node));
     cache = *Geral;
 
-    cache = *Geral;
-    *Geral = cache->prox;
-    (*Geral)->tamanho--;
-
-    free(cache);
+    if(cache->prox == NULL)
+    {
+        *Geral = criar_Lista();
+    }
+    else
+    {
+        cache = *Geral;
+        *Geral = cache->prox;
+        (*Geral)->tamanho--;
+    }
+    /*free(cache);*/
 }
 void Atualizar(Node **Geral, int select)
 {
 
 }
+
 
 int Buscar_Na_Lista(Node *Geral)
 {
@@ -151,11 +171,11 @@ int Buscar_Na_Lista(Node *Geral)
     return 1;
 }
 
+
 int CPF_NaBase(Node *Geral, char *cpf)
 {
     int validador = -1;
-    Node *referencia =(Node*) malloc(sizeof(Node));
-    referencia = Geral;
+    Node *referencia = Geral;
 
     if(Geral == NULL)
     {
@@ -182,20 +202,20 @@ int CPF_NaBase(Node *Geral, char *cpf)
         else
             validador = 1;
     }
-
     return validador;
 }
+
 
 Node* AtPos(Node *Geral, int select)
 {
     int opcao, matricula=0;
-    char *cpf = malloc(sizeof(char));
-    Node *cache = (Node*) malloc(sizeof(Node));
-    Node *referencia = (Node*) malloc(sizeof(Node));
-    referencia = Geral;
+    char cpf[12];
+    Node *cache = NULL;
+    Node *referencia = Geral;
 
     printf("\n Escolha: (1)- Matricula ou (2)- CPF, (9)- VOLTAR ou (0)- SAIR. ");
     opcao = getch();
+    setbuf(stdin,NULL);
 
     switch(opcao)
     {
@@ -204,11 +224,13 @@ Node* AtPos(Node *Geral, int select)
         exit(0);
         break;
     case '1':
-        printf("\nInforme a matricula: ");
+        setbuf(stdin,NULL);
+        printf("\n\n Informe a matricula: ");
         scanf("%d", &matricula);
         break;
     case '2':
-        printf("\nInforme o CPF: ");
+        setbuf(stdin,NULL);
+        printf("\n\n Informe o CPF: ");
         gets(cpf);
         break;
     case '9':
@@ -224,7 +246,7 @@ Node* AtPos(Node *Geral, int select)
         break;
     }
 
-    if(opcao == 1)
+    if(opcao == '1')
     {
         if(referencia->dados.matricula == matricula)
         {
@@ -238,16 +260,17 @@ Node* AtPos(Node *Geral, int select)
                 referencia = referencia->prox;
             }
             while(referencia != NULL && referencia->dados.matricula != matricula);
-        }
-        if(referencia == NULL)
-        {
-            return NULL;
+
+            if(referencia == NULL)
+            {
+                return NULL;
+            }
         }
     }
 
     else
     {
-        if(referencia->dados.cpf == cpf)
+        if(strcmp(referencia->dados.cpf, cpf) == 0)
         {
             return referencia;
         }
@@ -260,11 +283,35 @@ Node* AtPos(Node *Geral, int select)
             }
             while(referencia != NULL && strcmp(referencia->dados.cpf, cpf)!= 0);
         }
+
         if(referencia == NULL)
         {
             return NULL;
         }
     }
-
     return cache;
+}
+
+int isEmpty(Node *Geral)
+{
+    int aux = 0, validador;
+    Node *cache = Geral;
+
+    do
+    {
+        cache = cache->prox;
+        aux++;
+
+    }while(cache != NULL && aux < 2);
+
+    if(aux >= 2)
+    {
+        validador = 0;
+    }
+    else
+    {
+        validador = 1;
+    }
+
+    return validador;
 }
